140d13d75e032addb4beb752360a3453d4ae6d29
Echo-Gambino <nkb012pomm@gmail.com>
2020-05-05 17:27:19 -0600

    Fixed the bugs in regards to checkpoints

--- app/Checkpoint.cs
+++ app/Checkpoint.cs
@@ -3,37 +3,75 @@ using System;
 
 public class Checkpoint : Area2D
 {
-    [Export]
-    public bool active = false;
+    [Signal]
+    public delegate void Activated();
+    [Signal]
+    public delegate void Deactivated();
 
-    private AnimationPlayer animationPlayer = null;
+    [Export]
+    private PackedScene animationPlayer;
+    [Export]
+    private bool active = false;
+
+    private AnimationPlayer animationInstance = null;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        this.animationPlayer = this.GetNode<AnimationPlayer>("AnimationPlayer");
+        string animationName = (active) ? "CheckpointActive" : "CheckpointInactive";
 
-        string animationName = (active) ? "ActiveCheckpoint" : "InactiveCheckpoint";
-
-        this.animationPlayer.Play(animationName);
+        this.animationInstance = (AnimationPlayer)animationPlayer.Instance();
+        this.AddChild(this.animationInstance);
+        this.animationInstance.Play(animationName);
     }
 
-    public void Activate()
+    public void Activate(Node entity=null)
     {
         // If the checkpoint is active, then don't perform the animation (ActiveCheckpoint),
         // Otherwise, show an animation from inactive to active (CheckpointRecieved)
-        string animationName = (active) ? "ActiveCheckpoint" : "CheckpointRecieved";
+        string animationName = (active) ? "CheckpointActive" : "CheckpointRecieved";
         
-        this.animationPlayer.Play(animationName);
+        this.animationInstance.Play(animationName);
+
+        active = true;
+
+        EmitSignal("Activated", entity, this);
     }
 
-    public void Deactivate()
+    public void Deactivate(Node entity=null)
     {
         // If the checkpoint is active, show an animation from active to inactive (InactiveCheckpoint),
         // Otherwise, don't perform the animation (CheckpointLost),
-        string animationName = (active) ? "CheckpointLost" : "InactiveCheckpoint";
+        string animationName = (active) ? "CheckpointLost" : "CheckpointInactive";
 
-        this.animationPlayer.Play(animationName);
+        this.animationInstance.Play(animationName);
+
+        active = false;
+
+        EmitSignal("Deactivated", entity, this);
+    }
+
+    public void OnCheckpointBodyEntered(Node entity)
+    {
+        Player playerEntity = null;
+
+        try 
+        { 
+            playerEntity = (Player)entity; 
+        } 
+        catch (InvalidCastException)
+        {
+            // Failed to cast entity
+        }
+
+        if (playerEntity == null) { return; }
+
+        Activate(entity);
+    }
+
+    public void OnAnimationPlayerAnimationStarted(string animationName)
+    {
+        // ...
     }
 
 }
--- app/World.cs
+++ app/World.cs
@@ -11,14 +11,21 @@ public class World : Node2D
     [Signal]
     public delegate void NewPlayer();
 
-    private Area2D checkpoint;
+    private Vector2 spawnPosition;
+
+    private HashSet<string> listCheckpointNodeNames = new HashSet<string>();
+    private HashSet<string> listPlayerNodeNames = new HashSet<string>();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        this.checkpoint = this.GetNode<Area2D>("Checkpoint");
+        ConnectAll();
+
+        Position2D startPosition = this.GetNode<Position2D>("StartPosition");
+        this.spawnPosition = startPosition.Position;
+
         RemoveAllPlayers();
-        SpawnPlayer(this.checkpoint.Position);
+        SpawnPlayer(this.spawnPosition);
     }
 
     public override void _Process(float delta)
@@ -30,12 +37,57 @@ public class World : Node2D
             // then remove the current player (if there is any),
             // and spawn a new playable character.
             RemoveAllPlayers();
-            SpawnPlayer(this.checkpoint.Position);
+            SpawnPlayer(this.spawnPosition);
         }
+    }
+
+    public void ConnectAll()
+    {
+        HashSet<string> test = new HashSet<string>()
+        {
+            "TileMap_Spikes"
+        };
+
+        foreach (Node2D node in this.GetChildren())
+        {
+            bool t = test.Contains(node.Name);
+
+            if ((!this.IsConnected("NewPlayer", node, "OnNewPlayer")) && t)
+            {
+                this.Connect("NewPlayer", node, "OnNewPlayer");
+            }
+
+            Checkpoint c = AttemptCast<Checkpoint>(node);
+            if (c != null)
+            {
+                c.Connect("Activated", this, nameof(OnCheckpointActivate));
+                listCheckpointNodeNames.Add(c.Name);
+            }
+
+        }
+    }
+
+    public T AttemptCast<T>(Node node)
+    {
+        T output;
+        try
+        {
+            // Attempt to cast the node 
+            output = (T)(object)node;
+        }
+        catch (InvalidCastException)
+        {
+            output = default(T);
+        }
+
+        return output;
     }
 
     private void RemoveAllPlayers() 
     {
+        // Removes ALL the entries from list of player node names due to the method's purpose.
+        this.listPlayerNodeNames.Clear();
+
         // Create a regex string
         string regexString = String.Format("@?({0})@?.*", "Player");
         // Get a list of player node names
@@ -66,6 +118,7 @@ public class World : Node2D
             }
         }
     }
+
     private void SpawnPlayer(Vector2 position) 
     {
         // Start a new instance of the player object.
@@ -80,6 +133,33 @@ public class World : Node2D
         playerInstance.Show();
 
         EmitSignal("NewPlayer", playerInstance);
-        GD.Print(playerInstance.Name);
+
+        if (this.listPlayerNodeNames.Contains(playerInstance.Name))
+        {
+            throw new ApplicationException("New player instance name already within listPlayerNodeNames");
+        }
+
+        // Add the playerInstance's name into the list of player node names.
+        this.listPlayerNodeNames.Add(playerInstance.Name);
+    }
+
+    public void OnCheckpointActivate(Node entity, Checkpoint sender)
+    {
+        if (!this.listPlayerNodeNames.Contains(entity.Name)) { return; }
+
+        Checkpoint checkpoint;
+        foreach (string s in this.listCheckpointNodeNames)
+        {
+            checkpoint = this.GetNode<Checkpoint>(s);
+            
+            if (sender.Name == s) 
+            {
+                this.spawnPosition = checkpoint.Position;
+            }
+            else
+            {
+                checkpoint.Deactivate();
+            }
+        }
     }
 }
