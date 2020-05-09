bf0a4d43f308158c1293cd7087120c86770c620d
Echo-Gambino <nkb012pomm@gmail.com>
2020-05-09 01:33:40 -0600

    Made the game with the main menu, pause screen, and win state all in one instance

--- app/MainScene.cs
+++ app/MainScene.cs
@@ -1,8 +1,16 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class MainScene : Node2D
 {
+    public List<string> List_Of_World_Paths = new List<string>()
+    {
+        "World.tscn"
+    };
+
+    public List<PackedScene> LevelList = new List<PackedScene>();
+
     public bool IsPaused
     {
         get { return this.processStopFlag; }
@@ -12,20 +20,28 @@ public class MainScene : Node2D
     private bool IsPauseScreenActive
     {
         get { return this.pauseScreen.Visible; }
-        set { SetPauseScreen(value); }
+        set { SetPopupEntity(value, this.pauseScreen); }
     }
 
     private bool IsWinScreenActive
     {
         get { return this.winScreen.Visible; }
-        set { SetWinScreen(value); }
+        set { SetPopupEntity(value, this.winScreen); }
     }
 
-    private bool processStopFlag = false;
+    private bool IsMainMenuActive
+    {
+        get { return this.mainMenuScreen.Visible; }
+        set { SetPopupEntity(value, this.mainMenuScreen); }
+    }
+
+    private bool processStopFlag = true;
+    private int curWorldIndex = 0;
 
     private CanvasLayer canvasLayer = null;
     private PauseScreen pauseScreen = null;
     private WinScreen winScreen = null;
+    private MainMenuScreen mainMenuScreen = null;
     private World curGameWorld = null;
 
     // Called when the node enters the scene tree for the first time.
@@ -34,10 +50,21 @@ public class MainScene : Node2D
         this.canvasLayer = this.GetNode<CanvasLayer>("CanvasLayer");
         this.pauseScreen = this.canvasLayer.GetNode<PauseScreen>("PauseScreen");
         this.winScreen = this.canvasLayer.GetNode<WinScreen>("WinScreen");
-        this.curGameWorld = this.GetNode<World>("World");
+        this.mainMenuScreen = this.canvasLayer.GetNode<MainMenuScreen>("MainMenuScreen");
+
+        foreach (string scenePath in List_Of_World_Paths)
+        {
+            string formattedPath = String.Format("res://{0}", scenePath);
+            PackedScene scene = (PackedScene)ResourceLoader.Load(formattedPath);
+
+            this.LevelList.Add(scene);
+        }
+
+        // this.curGameWorld = this.GetNode<World>("World");
 
         this.IsPaused = this.processStopFlag;
-        this.IsPauseScreenActive = this.processStopFlag;
+        this.IsMainMenuActive = true;
+        this.IsPauseScreenActive = false;
         this.IsWinScreenActive = false;
 
     }
@@ -48,7 +75,7 @@ public class MainScene : Node2D
         GetTree().Paused = this.IsPaused;
 
         bool escPressed = Input.IsActionJustPressed("ui_cancel");
-        if (escPressed && (!this.IsWinScreenActive))
+        if (escPressed && (!this.IsWinScreenActive) && (!this.IsMainMenuActive))
         {
             // Disable the win screen (The "Pause" screen overrides the "Win" screen)
             this.IsWinScreenActive = false;
@@ -59,14 +86,60 @@ public class MainScene : Node2D
         }
     }
 
-    private void SetPauseScreen(bool enable)
+    private void ReloadWorld()
     {
-        SetPopupEntity(enable, this.pauseScreen);
+        FlushAllWorlds();
+        LoadWorld(this.curWorldIndex);
     }
 
-    private void SetWinScreen(bool enable)
+    private void LoadWorld(int index)
     {
-        SetPopupEntity(enable, this.winScreen);
+        World world = GetWorldFromLevelList(index) ?? throw new NullReferenceException("World is not valid");
+        
+        this.AddChild(world);
+        world.Connect("WinStateReached", this, nameof(OnWorldWinStateReached));
+        world.Show();
+    }
+
+    private void FlushAllWorlds()
+    {
+        List<World> worldList = new List<World>();
+        foreach (Node n in this.GetChildren())
+        {
+            try 
+            {
+                World w = (World)n;
+                worldList.Add(w);
+            }
+            catch (InvalidCastException)
+            {
+            }
+            
+        }
+
+        foreach (World w in worldList)
+        {
+            w.QueueFree();
+        }
+    }
+
+    private World GetWorldFromLevelList(int index)
+    {
+        if ((index < 0) && (index >= this.LevelList.Count)) { return null; }
+        
+        World output = null;
+
+        try
+        {
+            PackedScene scene = this.LevelList[index];
+            output = (World)scene.Instance();
+        }
+        catch (InvalidCastException)
+        {
+            output = null;
+        }
+
+        return output;
     }
 
     private void SetPopupEntity(bool enable, Popup popupObject)
@@ -96,7 +169,14 @@ public class MainScene : Node2D
     {
         this.IsWinScreenActive = false;
         this.IsPaused = false;
-        this.curGameWorld.Reset();
+        ReloadWorld();
+    }
+
+    public void OnWinScreenMainMenu()
+    {
+        this.IsPaused = true;
+        this.IsWinScreenActive = false;
+        this.IsMainMenuActive = true;
     }
 
     public void OnWinScreenQuitGame()
@@ -112,12 +192,31 @@ public class MainScene : Node2D
         this.IsPaused = false;
     }
 
+    public void OnPauseScreenMainMenu()
+    {
+        this.IsPaused = true;
+        this.IsPauseScreenActive = false;
+        this.IsMainMenuActive = true;
+    }
+
     public void OnPauseScreenQuitGame()
     {
         this.GetTree().Quit();
     }
     #endregion
 
-    
+    #region Main Menu Event Handlers
+    public void OnMainMenuScreenStartGame()
+    {
+        this.IsMainMenuActive = false;
+        this.IsPaused = false;
+
+        ReloadWorld();
+    }
+    public void OnMainMenuScreenQuitGame()
+    {
+        this.GetTree().Quit();
+    }
+    #endregion
 
 }
